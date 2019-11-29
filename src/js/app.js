1894d6bc60fe33f0a978c23b468ecee4ad36fabc
yralosev1988 <yralosev1988@gmail.com>
2019-11-29 22:07:12 +0300

    responsive header

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,17 @@
-import '../scss/app.scss';
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+  });
 
+  const menuToggle= document.querySelector("#menu-toggle");
+  const mobileNavContainer=document.querySelector('#mobile-nav');
+  menuToggle.onclick=function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active')
+  };
+});
